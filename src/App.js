b0ef66647f0a3c8a98a69d4b83b8a90ddac9c466
Fabio Moraes <fabio@elaboredigital.com.br>
2020-07-21 20:23:47 -0300

    update 2020-07-21

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
 import React from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = React.useState([]);
+  const [state, setState] = React.useState({
+    title: '',
+    url: '',
+    techs: [],
+  });
+  
+  React.useEffect(() => {
+    async function loadRepositories() {
+      const res = await api.get('repositories');
+
+      setRepositories(res.data);
+    }
+
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post('repositories', {
+      title: state.title,
+      url: state.url,
+      techs: state.techs,
+    });
+
+    const object = { data };
+
+    const convert = Object.values(object); // Transforma objeto em array
+
+    const dadosArray = convert.concat(repositories);
+
+    setRepositories(dadosArray.map(item => item));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const percorre = repositories.filter(item => item.id !== id);
+    setRepositories(percorre);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(item => (
+          <li key={item.id}>
+          {item.title}
+          <button onClick={() => handleRemoveRepository(item.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
+      <div>
+        <input type="text" onChange={e => setState({ ...state, title: e.target.value })} placeholder="titulo" />
+        <input type="text" onChange={e => setState({ ...state, url: e.target.value })} placeholder="URL GIT" />
+        <button type="button">Add Tech</button>
+      </div>
+      <div>
       <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
